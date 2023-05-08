898ab024b7b101676ed24f13338e6dbb9d780cc0
Emete Gift <emeteokeoghene@gmail.com>
2023-05-08 21:55:08 +0100

    commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 DATABASES['default'] = dj_database_url.config()
 
