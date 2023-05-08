2080eb1cda592ab3bbad94d1f5c0304a643d4832
Emete Gift <emeteokeoghene@gmail.com>
2023-05-08 21:37:36 +0100

    Modified

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-t8=f*j7jt^7u$(9goc8o^kfhj9h=tbuc^e(#3mrc3=lyi*mvfl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,7 +122,11 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+
+
+# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
