8e94e8f07b45a07560d64b88acde401c97e0741d
Emete Gift <emeteokeoghene@gmail.com>
2023-05-08 15:01:24 +0100

    Modified

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,15 @@
 # Build the poject
 echo "Bulid the project..."
-python3.10 -m pip install -r requirements.txt
+python3.10.11 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3.10.11 manage.py makemigrations --noinput
+python3.10.11 manage.py migrate --noinput
 
 echo "collect static..."
-python3.10 manage.py collectstatic --noinput --clear
+python3.10.11 manage.py collectstatic --noinput --clear
+
+
+# pip install -r requirements.txt
+# python3.10.11 manage.py collectstatic
 
